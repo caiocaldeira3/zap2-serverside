b89904dc7537c81454855b03b25973473b2673b6
caiocaldeira3 <caio.caldeira3@gmail.com>
2022-01-08 18:40:34 -0300

    Remove outdated postman collection and create an exit protocol

--- server/run.py
+++ server/run.py
@@ -1,3 +1,4 @@
+import atexit
 import dotenv
 
 from pathlib import Path
@@ -5,8 +6,17 @@ from pathlib import Path
 base_path = Path(__file__).resolve().parent
 dotenv.load_dotenv(base_path / ".env", override=False)
 
-from app import flask_app
+from app.models.device import Device
+
+from app import db
 from app import sio
+from app import flask_app
+
+def exit_handler():
+    Device.query.delete()
+    db.session.commit()
+
+atexit.register(exit_handler)
 
 if __name__ == '__main__':
     sio.run(flask_app, use_reloader=False)
